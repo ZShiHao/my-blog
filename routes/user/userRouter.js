0e1e5c8aa4cefa219d36e19473b578ed4dd4d7a2
Administrator <zhangshihaovv@163.com>
2023-03-02 18:23:07 +0800

    用户注册登录接口

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -2,11 +2,13 @@ import express from "express";
 import userSchema from "../../schema/user/userSchema.js";
 import {mongooseConnectDb} from "../../config/db.js";
 import bodyParser from "body-parser";
+import jwt from 'jsonwebtoken'
+import secret from "../../config/secret.js";
 
 const router=express.Router()
 
 const dbName='user'
-const collection='user_base'
+const collection='user_bases'
 
 //新用户注册
 router.post('/register',bodyParser.json(),async (req,res)=>{
@@ -32,10 +34,21 @@ router.post('/register',bodyParser.json(),async (req,res)=>{
         }
         const findRes=await Users.find(query)
         if (findRes.length===0){
+            const access_token=jwt.sign({
+                username: new_user.username,
+                phone:new_user.phone,
+                email:new_user.email,
+                avatar: new_user.avatar
+            },secret.privateKey,{
+                expiresIn:'2d'
+            })
             await Users.create(new_user)
             const resBody={
                 code:200,
-                message: '注册成功'
+                message: '注册成功',
+                data:{
+                    access_token
+                }
             }
             res.send(resBody)
         }else{
@@ -57,7 +70,11 @@ router.post('/register',bodyParser.json(),async (req,res)=>{
 //用户登录
 router.post('/login',async (req,res)=>{
     try {
-
+        const resBody={
+            code:500,
+            message:'123123'
+        }
+        res.send(resBody)
     } catch (e) {
 
     }
